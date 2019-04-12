a380164e304b1c45678870c6060806d8b63c76dc
kelldeveloped <48416779+kelldeveloped@users.noreply.github.com>
2019-04-12 16:48:15 -0400

    Added buttons to homepage and a JS file

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//console.log("Hello world!");
+
+var playText = document.getElementById('play-button-text');
+var pauseText = document.getElementById('pause-button-text');
+var playPause = document.getElementById('play-pause');
+var wavesVideo = document.getElementById('waves-video');
+
+//console.log(playText);
+//console.log(pauseText);
+//console.log(playPause);
+//console.log(wavesVideo);
+
+playPause.addEventListener("click", playControls);
+
+function playControls() {
+    if (wavesVideo.paused) {
+        wavesVideo.play();
+        playPause.innerHTML = "Play";
+    }
+    else wavesVideo.pause();
+    playPause.innerHTML = "Pause"; 
+};
